f8c251ed4b334b75656ee20bdf0562fe3d0fb1fd
Your Name <you@example.com>
2022-09-27 17:26:20 +0200

    Added packagejson stuff

--- seeds/champion.js
+++ seeds/champion.js
@@ -7,11 +7,12 @@ const ChampionsModel = require('../models/Champions.model');
 DDRAGONSERVICE
     .getDetailsChampions('Aatrox')
     .then(({ Aatrox }) => {
+        // console.log(Aatrox)
         const { key, name, title, skins, lore, allytips, enemytips, tags, stats } = Aatrox
-        console.log(typeof key.toString())
-
+        console.log(Aatrox)
         return ChampionsModel.insertMany(key, name, title, skins, lore, allytips, enemytips, tags, stats)
     })
+
     .then(() => console.log('todo ok'))
     .catch((err) => console.log(err))
 
